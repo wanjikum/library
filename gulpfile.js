6e6203cc5a34398e99b9ca50d611d12cbfc6b3c8
wanjikum <wanjikumillicent5@gmail.com>
2017-11-27 18:52:32 +0300

    Install wiredep

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
+
+// This helps us improve our code quality
 gulp.task('style', function(){
     return gulp.src(jsFiles)
     .pipe(jshint())
@@ -11,4 +13,16 @@ gulp.task('style', function(){
     })
     .pipe(jscs())
 );
+});
+
+// Helps us in injecting links to our index.html files
+gulp.task('inject', function(){
+    var wiredep = require('wiredep').stream; 
+    var options = {
+        bowerJson : require("./bower"),
+        directory: "./public/src/lib"
+    }
+    return gulp.src('./src/views/*.html')
+    .pipe(wiredep(options))
+    .pipe(gulp.dest('./src/views'))
 });
