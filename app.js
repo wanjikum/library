aeebc6e30c94784bc9d9ea503da7cbdf0036f066
wanjikum <wanjikumillicent5@gmail.com>
2017-11-29 20:27:57 +0300

    Add normal string in the ejs file

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ var port = process.env.PORT || 5000;
 // Set up a middleware for static folders
 app.use(express.static('public'));
 app.set('views', './src/views');
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({extname: '.hbs'}));
-app.set('view engine',  '.hbs');
+app.set('view engine',  'ejs');
 
 // Open up your browser and to the address bar write "http://localhost:5000/"
 app.get('/', function(req, res){
