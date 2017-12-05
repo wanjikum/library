462c840d9b3b65beaae34e9db5c0ab4b03baf4c5
wanjikum <wanjikumillicent5@gmail.com>
2017-12-05 15:09:48 +0300

    Implement sign up and store user in the database

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -7,6 +7,25 @@ var router = function(nav){
         .post(function(req, res){
             console.log('my role is to sign up');
             console.log("This is our req.body", req.body); 
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function(err, db ){
+                var collection = db.collection('users');
+                var user = {
+                    username: req.body.username,
+                    password: req.body.password 
+                }
+                collection.insert(user,  
+                    function(err, results){
+                        req.login(results.ops[0], function(){
+                            res.redirect('/auth/profile');
+                        })    
+                    })
+            });
+        })
+    authRouter.route('/profile')
+        .get(function(req, res){
+            res.json(req.user)
+
         })
     return authRouter;
 }
