1d6eec8f262bdda10c7a55bdeb140a69ed773b75
wanjikum <wanjikumillicent5@gmail.com>
2017-12-07 15:10:12 +0300

    Add a book service function

--- src/services/goodReadsService.js
+++ src/services/goodReadsService.js
@@ -1,10 +1,33 @@
- var goodReadService = function(){
-     var getBookById = function(id, cb){
-         cb(null, {description: 'Our Description'});
-     };
-     return {
+var http = require('http');
+var xml2js = require('xml2js'); 
+var parser = xml2js.Parser({explicitArray: false})
+ 
+ 
+var goodReadService = function(){
+    var getBookById = function(id, cb){
+        var options = {
+            host: 'www.goodreads.com',
+            path: '/book/show/656?format=xml&key=HKxayibz8Q0Y6mCUbrdDRA'
+        };
+
+        var callback = function(response){
+            var str = '';
+            response.on('data', function(chunk){
+                str += chunk;
+            });
+            response.on('end', function (){
+                parser.parseString(str, function(err, result){
+                    var nb = "Put a - sign instead of = in the html document if you would love to skip a html doc"
+                    result = 'This paragraph contains a lot of lines in the source code, but the browser ignores it'
+                    cb(null, result);
+                });
+            });
+        };     
+        http.request(options, callback).end();
+    };
+    return {
         getBookById: getBookById
     }
- };
+};
 
- module.exports = goodReadService;
+module.exports = goodReadService;
